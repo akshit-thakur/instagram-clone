e52fec72e33694e2b4285eae4cf7a1daa42ea6e5
Akshit Thakur <akshit.235@gmail.com>
2020-07-09 01:31:33 +0530

    Navbar-Layout-Completed

---
+++ src/components/home/homeComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return <div className="col-lg-10 offset-lg-2">Welcome to Instagram</div>;
+  }
+}
+
+export default Home;
---
+++ src/components/navigation/navigationComponent.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+const NavList = (props) => {
+  return props.list.map((item) => (
+    <li className="nav-item ">
+      <a className="nav-link" href={`${item}Component.js`}>
+        <img src={`icons/${item}.png`} alt={item} title={item} width={30} />
+      </a>
+    </li>
+  ));
+};
+
+class Navigation extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navItems: [
+        "search",
+        "home",
+        "explore",
+        "messages",
+        "notifications",
+        "profile",
+        "navDropdown",
+      ],
+    };
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top col-lg-8 offset-lg-2">
+        <a className="navbar-brand" href="index.js">
+          <img src="icons/logo.png" alt="Instagram" height={40} />
+        </a>
+
+        <ul className="nav navbar-nav navbar-right flex-row">
+          <NavList list={this.state.navItems} toggle={this.state.toggle} />
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
